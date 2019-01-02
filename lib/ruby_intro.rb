ea6469cb56eaeb9c94dcf7d5fb5bdb75c8349ba6
avoneiff <avoneiff@tamu.edu>
2019-01-02 02:48:19 +0000

    homework 1 commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,50 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0){|sum, x| sum + x }
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.max(2).inject(0 , :+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? {|a, b| (a+b)==n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  
+ /^[bcdfghjklmnopqrstvwxyz]/i =~ s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+ 
+ /^(?:[01]*0)?0$/ =~s
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize (isbn, price)
+    raise ArgumentError if price <= 0
+    raise ArgumentError if isbn.length == 0
+    # variables
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    sprintf("$%.2f", @price)
+    
+  end
+
 end
